3cc9dfa92a81621bf4c3835ed0b8940dafddd3bb
Conner Reeves <ConnerReeves@gmail.com>
2016-07-06 15:36:33 +0000

    remove logging (causing startup issues)

--- food-bot.py
+++ food-bot.py
@@ -1,12 +1,11 @@
+#!/usr/bin/python
+
 from scapy.all import *
 import datetime
-import logging
 import picamera
 import pyimgur
 import requests
 import time
-
-logging.getLogger().setLevel(logging.INFO)
 
 DASH_BUTTON_MAC = '74:c2:46:72:b2:fc'
 FLOW_API_TOKEN = '124510b8e412d27e3397bc86696440bf' #Sandbox
@@ -17,21 +16,19 @@ IMGUR_CLIENT_ID = 'b933b672619f23e'
 def listen(pkt):
     if pkt[ARP].op == 1:
         if pkt[ARP].hwsrc == DASH_BUTTON_MAC: #food-bot button pressed
-            logging.info('button press detected')
-            post_food();
+            timestamp = str(datetime.datetime.now())[:19]
+            print 'press: ' + timestamp
+            post_food(timestamp);
 
-def post_food():
+def post_food(timestamp):
     #take food image
     camera = picamera.PiCamera()
     camera.capture(IMAGE_PATH)
     camera.close()
-    logging.info('image captured')
 
     #upload food image
-    timestamp = str(datetime.datetime.now())[:19]
     im = pyimgur.Imgur(IMGUR_CLIENT_ID)
-    image = im.upload_image(IMAGE_PATH, title='FoodBot ' + timestamp)
-    logging.info('image uploaded')
+    image = im.upload_image(IMAGE_PATH, title=timestamp)
 
     #post message to flowdock
     payload = {
@@ -40,7 +37,5 @@ def post_food():
     }
 
     requests.post(FLOW_MESSAGE_URL, data = payload)
-    logging.info('post made')
 
-logging.info('food-bot started')
 print sniff(prn=listen, filter="arp")
